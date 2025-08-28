0d0179245872e858294ccfd18b4054c30f07dd60
Martin Monkman <monkmanmh@yahoo.ca>
2025-08-28 11:23:27 -0700

    images, dams not bida

---
+++ _publishing_notes.R
@@ -0,0 +1,14 @@
+## publishing
+
+# a two-step process:
+  
+#  1. render
+
+bookdown::render_book()
+
+#2. publish to bookdown.org
+
+bookdown::publish_book(name = "DataAnalyticsCodingFundamentals", account = "martin_monkman")
+
+
+
